a262a61e6fabe863718305549ea6438b5e2ff528
Pau <Pau@Temporals-MacBook-Pro.local>
2017-08-30 13:25:01 +0200

    Basic structure completed

--- README.rst
+++ README.rst
@@ -1,8 +1,14 @@
 Emb4class
-=======================
+=========
 
-A package used to compare between different word embeddings (including their hyperparameters)
+A Python 3, tensorflow based package used to compare between different word embeddings
+(including their hyperparameters)
 for short text classification. Some sample datasets are provided.
 
+Requirements:
+
+python >= 3.5, numpy >= 1.12, tensorflow >= 0.11, nltk >= 3.2.3, sklearn >= 0.18.2, matplotlib >= 2.0, seaborn >= 0.7.1
 ----
 Examples:
+
+[WIP]
--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
 from distutils.core import setup
+from setuptools import find_packages
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='emb4class',
+      version='0.1',
+      description='Word embedding benchmark for short text classification',
+      long_description = readme(),
+      url='http://github.com/PauBatlle/emb4class',
+      author='Pau Batlle',
+      author_email='pau.batlle@estudiant.upc.edu',
+      license='MIT',
+      install_requires = ["numpy", "tensorflow", "matplotlib", "seaborn", "scikit-learn", "nltk"],
+      packages=find_packages(),
+      python_requires = '>=3.3',
+      classifiers=['Development Status :: 3 - Alpha'])
